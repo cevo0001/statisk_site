967583b5b846164a8b2a3fdb72594e24fe9783f0
Celine Sophie <cevo0001@stud.kea.dk>
2025-01-30 11:57:19 +0100

    dynamiskJingle - opgave - done

---
+++ single.js
@@ -0,0 +1,35 @@
+let productId = 1525;
+let product_container = document.querySelector(".product_container");
+
+fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+    .then(Response => Response.json())
+    .then((data) => {
+        product_container.innerHTML = `
+            <div class="kasse-1">
+                <img src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp" alt="produktsbillede">
+            </div>
+    
+            <div class="kasse-2">
+                <h2 class="produkt"><b>${data.productdisplayname}</b></h2>
+                <h4>Unisex</h4>
+                <h3>ID</h3>
+                <h4>1525</h4>
+                <h3>Color</h3>
+                <h4>Navy Blue</h4>
+                <br>
+                <h2 class="produkt">${data.brandname}</h2>
+                <h3><b>${data.price}</b></h3>
+            </div>
+    
+            <div class="kasse-3">
+                <h3>Size</h3>
+                <select name="Sizes" id="select1">
+                    <option>S</option>
+                    <option>M</option>
+                    <option>L</option>
+                </select>
+                <a href="product.html"><h3 class="add">Add to Basket</h3></a>
+                <a href="product.html" class="checkout"><h3><u>Go to checkout</u></h3></a>
+            </div>
+            `;
+    })
