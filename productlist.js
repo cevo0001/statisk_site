98ec286896fd3e5f10de2ff592487bb905711da6
Celine Sophie <cevo0001@stud.kea.dk>
2025-01-31 10:42:05 +0100

    liste

--- productlist.js
+++ productlist.js
@@ -3,7 +3,7 @@ let product_list_container = document.querySelector(".product_list_container");
 
 fetch(`https://kea-alt-del.dk/t7/api/products/`)
     .then(response => response.json())
-    .then((data) => showList (data));
+    .then((data) => showList(data));
 
     function showList(products){ 
         console.log(products);
@@ -12,7 +12,7 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
             `<a href="product.html" class="box box-1">
             <img src="img/navy_backpack${productId}.webp" alt="brand navn">
             <h4>${product.productdisplayname}</h4>
-            <p>${product.brandname}, ${product.articletype}<br>, DKK <b>${product.price}</b> <br> <u>more info...</u></p>
+            <p>${product.brandname}, ${product.articletype}<br> DKK <b>${product.price}</b> <br> <u>more info...</u></p>
         </a>
         `;}).join("");
         console.log(markup);
