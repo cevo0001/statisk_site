62d653cc59b2cfa5dd865a6410fa564f45eb244e
Celine Sophie <cevo0001@stud.kea.dk>
2025-01-31 11:03:18 +0100

    productlist - done

--- productlist.js
+++ productlist.js
@@ -9,7 +9,11 @@ fetch(`https://kea-alt-del.dk/t7/api/products/`)
         console.log(products);
         let markup = "";
         products.map(product=>{
-markup+=`test`
+markup+=` <a href="product.html" class="box box-1">
+                  <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="produktsbillede" />
+                <h4>${product.productdisplayname}</h4>
+                <p> ${product.brandname}, ${product.articletype} <br> DKK <b>${product.price}</b> <br> <u>more info...</u></p>
+            </a>`
         }).join("");
         product_list_container.innerHTML=markup;
 
