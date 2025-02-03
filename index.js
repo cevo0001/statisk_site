be278757428ad2f6275802055461a724c32c998b
Celine Sophie <cevo0001@stud.kea.dk>
2025-02-03 14:04:34 +0100

    index- data done

---
+++ index.js
@@ -0,0 +1,15 @@
+let season_list_container = document.querySelector(".season_list_container");
+
+fetch(`https://kea-alt-del.dk/t7/api/seasons/`)
+    .then(response => response.json())
+    .then((data) => showList(data));
+
+    function showList(seasons){ 
+        console.log(seasons);
+        const markup = seasons.map(
+        (season) =>`<div class="spring">
+                <a href="productlist.html">${season.season}</a>
+            </div>`
+        ).join("");
+        season_list_container.innerHTML=markup;
+        }
