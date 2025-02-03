88a50a891aa02b6025b05a81519151f44f13d737
Celine Sophie <cevo0001@stud.kea.dk>
2025-02-03 12:39:14 +0100

    Productlist- rettet markup

--- productlist.js
+++ productlist.js
@@ -1,20 +1,18 @@
 
 let product_list_container = document.querySelector(".product_list_container");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
     .then(response => response.json())
     .then((data) => showList(data));
 
     function showList(products){ 
         console.log(products);
-        let markup = "";
-        products.map(product=>{
-markup+=` <a href="product.html" class="box box-1">
+        const markup = products.map(
+        (product) =>` <a href="product.html" class="box box-1">
                   <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="produktsbillede" />
                 <h4>${product.productdisplayname}</h4>
                 <p> ${product.brandname}, ${product.articletype} <br> DKK <b>${product.price}</b> <br> <u>more info...</u></p>
             </a>`
-        }).join("");
+        ).join("");
         product_list_container.innerHTML=markup;
-
         }
